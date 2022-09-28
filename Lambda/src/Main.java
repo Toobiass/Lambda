898581cf0bbd118d03e3a1d97904cf9a17c035c1
tobia <tobias.hoeller2005@gmail.com>
2022-09-28 18:39:54 +0200

    Aufgabe 3 fast fertig

--- Lambda/src/Main.java
+++ Lambda/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         NumberTester x = new NumberTester("LambdaText.txt");
+        x.setOddEvenTester((n)->(n % 2) == 0);
+        x.setPrimeTester((n)->{
+            boolean primzahl=true;
+
+            while(n>0){
+
+                if(n%2==0){
+                    primzahl=false;
+
+                }
+                else if(n%3==0){
+                    primzahl=false;
+
+                }
+
+            }return primzahl;});
+
+        //x.setPalindromeTester((n)->(n % 2) == 0); //palindrometester
+
+        x.testFile();
     }
 }
--- Lambda/src/NumberTester.java
+++ Lambda/src/NumberTester.java
@@ -4,34 +4,12 @@ import java.util.Scanner;
 
 public class NumberTester
 {
+
+    private String fileName;
     private NumberTest oddTester;
     private NumberTest primeTester;
     private NumberTest palindromeTester;
 
-
-    public NumberTester ( String fileName ) {
-      try{
-          Scanner s = new Scanner(new File(fileName));
-
-          String firstline = s.nextLine();
-
-          for (int i = 0; i < Integer.parseInt(firstline); i++) {
-               String tonumber= s.nextLine();
-
-              String[] splitted = tonumber.split(" ");
-              int zahl1 =Integer.parseInt(splitted[0]);int zahl2 =Integer.parseInt(splitted[1]);
-
-
-          }
-          while(s.hasNext()){
-              System.out.println(s.nextLine());
-          }
-      } catch (FileNotFoundException e) {
-          e.printStackTrace();
-      }
-
-
-    }
     public void setOddEvenTester ( NumberTest oddTester )
     {
         this.oddTester = oddTester;
@@ -43,9 +21,51 @@ public class NumberTester
         this.palindromeTester = palindromeTester ;
     }
     public void testFile ( ) {
+        try{
+            Scanner s = new Scanner(new File(fileName));
 
-        NumberTest oddTester = (n) -> (n % 2) == 0;
+            String firstline = s.nextLine();
 
+            for (int i = 0; i < Integer.parseInt(firstline); i++) {
+                String tonumber= s.nextLine();
+
+                String[] splitted = tonumber.split(" ");
+                int zahl1 =Integer.parseInt(splitted[0]);int zahl2 =Integer.parseInt(splitted[1]);
+                System.out.println(zahl1+"!"+zahl2);
+
+                switch (zahl1){
+                    case 1:
+                        if(oddTester.testNumber(zahl2)){
+                            System.out.println("ODD");
+                        }else{
+                            System.out.println("EVEN");
+                        }
+                        break;
+                    case 2:
+                        if(primeTester.testNumber(zahl2)){
+                            System.out.println("PRIME");
+                        }else{
+                            System.out.println("NO PRIME");
+                        }
+                        break;
+                    case 3:
+                        break;
+                }
+
+
+            }
+            while(s.hasNext()){
+                System.out.println(s.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
+    public NumberTester ( String fileName ) {
+
+this.fileName = fileName;
+
+    }
+
 }
